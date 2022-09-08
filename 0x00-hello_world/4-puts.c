7cbf72d5d8547caa769f693180b4ff0436c83544
Timmy-Ezekiel <timilehinoluwatuyi98@gmail.com>
2022-09-08 19:08:09 +0000

    First C code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+/**
+  * puts - This function prints the output to the screen
+  * @puzzle - This function takes a set of character in it.
+  *
+  * Return: This function return the string.
+  */
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+}
