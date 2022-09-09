d5defa3ade4dc12dd6df0f41617cbfcab6dafee0
Timmy-Ezekiel <timilehinoluwatuyi98@gmail.com>
2022-09-09 16:11:49 +0000

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Entry Point
+  *
+  * Return: (0) Success
+  *
+ **/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+		putchar('\n');
+return (0);
+}
