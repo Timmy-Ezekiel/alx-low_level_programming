264a53dbd766cfccb429fd1ae68823f12dee73d9
Timmy-Ezekiel <timilehinoluwatuyi98@gmail.com>
2022-09-09 15:51:04 +0000

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: (0) Sucess
+  *
+ **/
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+		putchar('\n');
+return (0);
+}
