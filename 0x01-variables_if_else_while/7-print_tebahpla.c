9761270da2fdb5e5289421c2757c13b288f621af
Timmy-Ezekiel <timilehinoluwatuyi98@gmail.com>
2022-09-09 16:54:04 +0000

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+	* main - Entry point
+	*
+	* Return: (0) Success
+	*
+	**/
+
+int main(void)
+{
+	char ch;
+
+			for (ch = 'Z'; ch >= 'A'; ch--)
+			{
+				putchar(ch);
+			}
+				putchar('\n');
+
+return (0);
+}
