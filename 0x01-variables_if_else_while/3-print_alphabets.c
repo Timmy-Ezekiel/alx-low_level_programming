d6e1db0c5b225167572f57c9916657d1b3f27817
Timmy-Ezekiel <timilehinoluwatuyi98@gmail.com>
2022-09-09 15:54:35 +0000

    Done

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,10 +12,15 @@ int main(void)
 {
 	char ch;
 
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	};
+
 	for (ch = 'A'; ch <= 'Z'; ch++)
 	{
 		putchar(ch);
-	}
+	};
 		putchar('\n');
 return (0);
 }
