b061b5df3d4e30d3dee90cf8340c13f48c408d33
Timmy-Ezekiel <timilehinoluwatuyi98@gmail.com>
2022-09-09 16:41:54 +0000

    Done task 06

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main- Entry Point
+  *
+  * Return: (0) Sucess
+  *
+ **/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+	}
+		putchar('\n');
+return (0);
+}
