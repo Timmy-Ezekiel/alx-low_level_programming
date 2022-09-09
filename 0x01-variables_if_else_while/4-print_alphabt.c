cd618c53115ac0c97e667ed0ce1a138a51aeab26
Timmy-Ezekiel <timilehinoluwatuyi98@gmail.com>
2022-09-09 16:06:27 +0000

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main- Entry point
+  *
+  * Return: (0) Success
+  *
+ **/
+
+int main(void)
+{
+	 char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'q' || ch == 'e')
+		{
+			;
+		}
+		else
+		{
+			putchar(ch);
+		}
+	}
+		putchar('\n');
+return (0);
+}
