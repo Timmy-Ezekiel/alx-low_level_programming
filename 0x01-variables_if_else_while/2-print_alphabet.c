e4f3cf51bf0dc4d6e246e6430f8dc33dc6f023c9
Timmy-Ezekiel <timilehinoluwatuyi98@gmail.com>
2022-09-09 15:36:00 +0000

    Done

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,20 +9,9 @@
  **/
 int main(void)
 {
-	char alp1[13], yes[13];
-	int x;
+	char ch;
 
-	alp1[13] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
-	for (x = 0; x <= 13; x++)
-	{
-		putchar(alp1[x]);
-	}
-	yes[13] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-
-	for (x = 0; x <= 13; x++)
-	}
-		putchar(yes[x]);
-	}
-
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
 	return (0);
 }
