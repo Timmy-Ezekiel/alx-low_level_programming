bd3e9c2a826148caa06fe4187722bdcc7cec710c
Timmy-Ezekiel <timilehinoluwatuyi98@gmail.com>
2022-09-13 14:14:55 +0000

    Done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+	*_abs - prints the absolute value of an interger
+	*
+	* @x : integer value
+	*
+	* Return: y
+	*
+	**/
+
+int _abs(int x)
+{
+	int y;
+
+	y = abs(x);
+return (y);
+}
