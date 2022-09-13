4407ef2468d2b0d7ae0bf9d0ebb2a04d0867b23e
Timmy-Ezekiel <timilehinoluwatuyi98@gmail.com>
2022-09-13 08:14:37 +0000

    Done task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void printPutchar(void);
+/**
+	* main - Entry Point
+	*
+	* Return: (0) Success
+	*
+	**/
+int main(void)
+{
+	printPutchar();
+return (0);
+}
+
+/**
+	* printPutchar()- prints the string _putchar
+	*
+	* Return: (void) Success
+	*
+	**/
+void printPutchar(void)
+{
+	printf("_putchar\n");
+return;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+#This directory contains files written during tasks for functions and nested loops
+
+* 0-putchar.c : This file only print \_putchar to Standard output.
