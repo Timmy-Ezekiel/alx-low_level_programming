b4b431f54f63a4b6ee540dcc0bb571ddb8708ff5
Timmy-Ezekiel <timilehinoluwatuyi98@gmail.com>
2022-09-15 09:52:00 +0000

    Done task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * print_line - prints undescore character "_"
+  *
+  * @n : integer that specifies the number of undescore.
+  *
+  */
+
+void print_line(int n)
+{
+	int x;
+
+	x = 0;
+	if (n > 0)
+	{
+		while (x <= n)
+		{
+			_putchar('_');
+			x++;
+		}
+	}
+	_putchar('\n');
+}
