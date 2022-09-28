daf46cebf748b31dbe4288ce7b58d7aa31ea5aeb
Timmy-Ezekiel <timilehinoluwatuyi98@gmail.com>
2022-09-28 14:30:28 +0000

    String Reverse function

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * _print_rev_recursion - Prints reverse of a string
+  *
+  * @s : string pointer
+  *
+ **/
+
+void _print_rev_recursion(char *s)
+{
+	if ((*s == '\0'))
+	{
+		return;
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
