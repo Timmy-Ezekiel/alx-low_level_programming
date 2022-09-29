714dfb351bb4c66c5840e3c344e544c359d09b5f
Timmy-Ezekiel <timilehinoluwatuyi98@gmail.com>
2022-09-29 09:35:46 +0000

    Prime Number checker C

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+int is_prime_function(int n, int x);
+/**
+  *is_prime_number - Primes 1 if a number is prime otherwise.
+  *
+  * @n : Integer number to be checked.
+  *
+  *Return: 0 or 1 Success
+  *
+ **/
+
+int is_prime_number(int n)
+{
+	int x;
+	
+	if (n <= 1)
+	{
+		return 0;
+	}
+	x = is_prime_function(n, 2);
+	return (x);
+}
+
+/**
+  * is_prime_function - Checkes for possible factors of n
+  *
+  * @n : integer to be checked
+  * @x : numbers to start checking from
+  *
+  * Return: 0 or 1
+  *
+ **/
+
+int is_prime_function(int n, int x)
+{
+	if ((n % x == 0) && (x != n))
+	{
+		return (0);
+	}
+	else if (x == n)
+	{
+		return (1);
+	}
+	else
+	{
+		return (is_prime_function(n, x + 1));
+	}
+}
