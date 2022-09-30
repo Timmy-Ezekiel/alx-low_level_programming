a5c5e65393b7737ac78e48b14dec4b3c81066781
Timmy-Ezekiel <timilehinoluwatuyi98@gmail.com>
2022-09-30 12:52:42 +0000

    Readme And First C Argv file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Entry point
+  *
+  * @argc : Argument Counts (Int parameter)
+  * @argv : Arguments (String Pointer)
+  *
+  * Return: integer (0) Success
+  *
+ **/
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,5 @@
+This directory contains files that contains argv and argc argument for C programming.
+
+* argc - integer parameter which means Argument count.
+* argv - pointer to strings of charcaters.
+
