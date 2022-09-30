42dd7eafceb2cfbb319fd5f275210b51938105b7
Timmy-Ezekiel <timilehinoluwatuyi98@gmail.com>
2022-09-30 14:04:55 +0000

    Number Adder 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Entry Point
+  *
+  * @argc : Argument counts
+  * @argv : Argumes pointer
+  *
+  * Return: intger (0) Success
+  *
+ **/
+
+int main(int argc, char **argv)
+{
+	int x, sum;
+	int convert;
+
+	sum = 0;
+	for (x = 1; x < argc; x++)
+	{
+		convert = atoi(argv[x]);
+		if (convert == 0)
+		{
+			printf("Error\n");
+			return (0);
+		}
+		sum += convert;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
