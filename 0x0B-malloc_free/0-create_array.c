d6aae9e66e29730d33eeb29607765cb7c4b2faf3
Timmy-Ezekiel <timilehinoluwatuyi98@gmail.com>
2022-10-03 16:38:04 +0000

    Initialization of Malloc task- Readme, header file and first (Create Array)

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * create_array - This function creates an array.
+  *
+  * @size : An Unsigned int character(size of array);
+  * @c : Char to intialize each address with
+  *
+  * Return: A pointer to this address.
+  *
+ **/
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int x;
+
+	if (size == 0)
+		return (NULL);
+	ptr = malloc((size - 1) * sizeof(char));
+	for (x = 0; x <= size; x++)
+	{
+		*(ptr + x) = c;
+	}
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+#This Directory is desgined to solve the ALX Memory Allocation Task(MALLOC)
+* Contains a total of 7 tasks. 5 Mandatory and 2 Advance.
