1952ab70513d95dc3adc9f2afe7de68cab8f5a18
Timmy-Ezekiel <timilehinoluwatuyi98@gmail.com>
2022-10-04 21:56:35 +0000

    Updated 100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  *argstostr - concatenates all it arguments.
+  *
+  * @ac : integer argument.
+  * @av : arguments.
+  *
+  * Return: a pointer
+  *
+ **/
+
+char *argstostr(int ac, char **av)
+{
+	char *concat;
+	int x, y, a, sum = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (x = 0; x < ac; x++)
+	{
+		y = 0;
+		while (av[x][y] != '\0')
+		{
+			sum++;
+			y++;
+		}
+	}
+	concat = malloc((sum + ac) * sizeof(char));
+	if (concat == NULL)
+		return (NULL);
+	a = 0;
+	for (x = 0; x < ac; x++)
+	{
+		y = 0;
+		while (1)
+		{
+			if (av[x][y] == '\0')
+			{
+				concat[a] = '\n';
+				a++;
+				break;
+			}
+			concat[a] = av[x][y];
+			a++;
+			y++;
+		}
+	}
+	return (concat);
+}
