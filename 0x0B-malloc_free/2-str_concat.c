0272425feb7558f4aa5ac6f975489bdd5313db51
Timmy-Ezekiel <timilehinoluwatuyi98@gmail.com>
2022-10-04 07:02:51 +0000

    Updated str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,29 +18,19 @@ char *str_concat(char *s1, char *s2)
 	int x, y; /*Variables to store the length of strings*/
 	int a, b; /*Varibles for loops*/
 	/*Get the length of the two strings*/
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	x = 0;
-	if (s1 != NULL)
+	while (s1[x] != '\0')
 	{
-		while (s1[x] != '\0')
-		{
-			x++;
-		}
-	}
-	else
-	{
-		x = 0;
+		x++;
 	}
 	y = 0;
-	if (s2 != NULL)
+	while (s2[y] != '\0')
 	{
-		while (s2[y] != '\0')
-		{
-			y++;
-		}
-	}
-	else
-	{
-		y = 0;
+		y++;
 	}
 	/*Allocate memory to the new string*/
 	concat = malloc((x + y + 1) * sizeof(char));
