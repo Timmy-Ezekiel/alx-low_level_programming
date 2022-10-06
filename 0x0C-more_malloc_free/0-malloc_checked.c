01965304b6dbe5a7763cbedda8f360482ab483f2
Timmy-Ezekiel <timilehinoluwatuyi98@gmail.com>
2022-10-06 16:59:39 +0000

    Updated 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
 
 /**
@@ -12,8 +13,7 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
-	ptr = malloc(b);
+	void *ptr = malloc(b);
 	if (ptr == NULL)
 		exit(98);
 	return (ptr);
