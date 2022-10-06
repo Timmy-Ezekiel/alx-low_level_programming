2b25ed15acf4485d1fb8f9a77cb78746bebdf7f7
Timmy-Ezekiel <timilehinoluwatuyi98@gmail.com>
2022-10-06 17:58:24 +0000

    Updated

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -23,10 +23,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
-	for (x = 0; x < nmemb; x++)
-	{
+	for (x = 0; x < (nmemb * size); x++)
 		ptr[x] = 0;
-	}
 	allocate = ptr;
 	return (allocate);
 }
