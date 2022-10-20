1417d2cc3438332bd93bef6d24616f7242fc3641
Timmy-Ezekiel <timilehinoluwatuyi98@gmail.com>
2022-10-20 21:02:33 +0000

    Done last task and updated 2nd to last

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@
 void printer(void) __attribute__((constructor));
 void printer(void)
 {
-	printf("You're beat! and yet, you must allow\n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
