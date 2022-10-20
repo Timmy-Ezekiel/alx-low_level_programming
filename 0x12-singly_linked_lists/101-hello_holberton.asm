396b9d597df5f6914df5e8a19fb8cdd0c31f94b0
Timmy-Ezekiel <timilehinoluwatuyi98@gmail.com>
2022-10-20 21:17:56 +0000

    Done

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,26 +1,25 @@
-.MODEL SMALL 
-.STACK 100H 
-.DATA 
-  
-;The string to be printed 
-STRING DB 'Hello, Holberton\n', '$'
-  
-.CODE 
-MAIN PROC FAR 
- MOV AX,@DATA 
- MOV DS,AX 
-  
- ; load address of the string 
- LEA DX,STRING 
-  
- ;output the string
- ;loaded in dx 
- MOV AH,09H
- INT 21H 
-  
- ;interrupt to exit
- MOV AH,4CH
- INT 21H 
-  
-MAIN ENDP 
-END MAIN 
+	.file	"main.c"
+	.section	.rodata
+.LC0:
+	.string	"Hello, Holberton"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$.LC0, %edi
+	call	puts
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 4.8.4-2ubuntu1~14.04.4) 4.8.4"
+	.section	.note.GNU-stack,"",@progbits
