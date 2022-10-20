433b702882e3bc0be691af418e23b97bfb77e4bd
Timmy-Ezekiel <timilehinoluwatuyi98@gmail.com>
2022-10-20 21:21:52 +0000

    Updated

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,25 +1,7 @@
-	.file	"main.c"
-	.section	.rodata
-.LC0:
-	.string	"Hello, Holberton"
-	.text
-	.globl	main
-	.type	main, @function
-main:
-.LFB0:
-	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	movl	$.LC0, %edi
-	call	puts
-	popq	%rbp
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE0:
-	.size	main, .-main
-	.ident	"GCC: (Ubuntu 4.8.4-2ubuntu1~14.04.4) 4.8.4"
-	.section	.note.GNU-stack,"",@progbits
+.data
+str: .asciiz "Hello, Holberton\n"
+
+.text
+li $v0, 4
+la $a0, str
+syscall
