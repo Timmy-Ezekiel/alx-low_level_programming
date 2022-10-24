c0a9f6e7add09f803e21ccd5b3e8352f90a9835d
Timmy-Ezekiel <timilehinoluwatuyi98@gmail.com>
2022-10-24 22:31:24 +0000

    Task 05 uploaded

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+  *free_listint2 - function free a listint_t list
+  *
+  *@head : The head of the singly linked list
+  *
+ **/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *nextnode;
+
+	while (*head != NULL)
+	{
+		nextnode = (*head)->next;
+		free(*head);
+		*head = nextnode;
+	}
+	*head = NULL;
+}
