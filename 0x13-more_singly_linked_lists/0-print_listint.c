0a89dd00516e8133962d093bf94cfed32258a950
Timmy-Ezekiel <timilehinoluwatuyi98@gmail.com>
2022-10-25 00:55:33 +0000

    updated 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,16 +9,15 @@
   *Return: Number of nodes
   *
  **/
-
 size_t print_listint(const listint_t *h)
 {
-	size_t Nnodes = 0;
+	size_t nnodes = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		Nnodes += 1;
+		nnodes++;
 	}
-	return (Nnodes);
+	return (nnodes);
 }
